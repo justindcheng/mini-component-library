6db046ac5cab4d51f9d17d4d0fcdc4c229332031
Justin Cheng <justinchenghoching@gmail.com>
2022-11-06 18:25:51 -0500

    progressbar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,52 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+  return <Wrapper role="progressbar" aria-valuenow={value} style={styles}>
+      <BarWrapper>
+        <Bar style={{ '--width': value + '%' }}/>
+      </BarWrapper>
+    </Wrapper>;
 };
+
+const SIZES = {
+  large: {
+    '--height': '24px',
+    '--padding': '4px',
+    '--borderRadius': '8px'
+  },
+  medium: {
+    '--height': '12px',
+    '--padding': '0px',
+    '--borderRadius': '4px'
+  },
+  small: {
+    '--height': '8px',
+    '--padding': '0px',
+    '--borderRadius': '4px'
+  }
+}
+
+const Wrapper = styled.div`
+  width: 370px;
+  background: ${COLORS.transparentGray15};
+  height: var(--height);
+  padding: var(--padding);
+  border-radius: var(--borderRadius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+
+const BarWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+const Bar = styled.div`
+  width: var(--width);
+  height: 100%;
+  background: ${COLORS.primary};
+`
 
 export default ProgressBar;
