dc26d56ac7160eff465de0ad76f9d442f67880fe
Justin Cheng <justinchenghoching@gmail.com>
2022-11-06 23:06:18 -0500

    fix select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,18 +9,20 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return <Wrapper style={{'--borderRadius': '8px'}}>
-      <span>{displayedValue}</span>
-      <Icon id='chevron-down' size='12px' style={{position: 'relative', display: 'inline-block', 'margin-left': '24px'}}/>
       <StyledSelect value={value} onChange={onChange}>
         {children}
       </StyledSelect>
+      <span>{displayedValue}</span>
+      <IconWrapper>
+        <Icon id='chevron-down' size='24px' strokeWidth='2'/>
+      </IconWrapper>
     </Wrapper>
   ;
 };
 
 const Wrapper = styled.div`
   position: relative;
-  padding: 12px 16px;
+  padding: 12px 52px 12px 16px;
   border-radius: var(--borderRadius);
   width: fit-content;
   color: ${COLORS.gray700};
@@ -35,16 +37,29 @@ const Wrapper = styled.div`
   }
 `
 
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 12px;
+  margin: auto;
+  height: fit-content;
+`
+
 const StyledSelect = styled.select`
   position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   border: none;
   border-radius: var(--borderRadius);
-  opacity: 0%;
+  color: transparent;
+  background-color: transparent;
 
   & > option {
-    opacity: 100%;
+    color: ${COLORS.black};
+    background-color: ${COLORS.transparentGray15};
   }
 `
 
