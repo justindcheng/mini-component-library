392086ca66f75001ff8d16afa587ed744ae7fd1f
Justin Cheng <justinchenghoching@gmail.com>
2022-11-08 00:37:00 -0500

    iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,81 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = {
+    '--width': width + 'px',
+    ...SIZES[size]
+  }
+  return <Wrapper style={styles}>
+      <VisuallyHidden><label>{label}</label></VisuallyHidden>
+      <IconWrapper><Icon id={icon} strokeWidth={styles['iconStrokeWidth']} size={styles['--fontSize']} /></IconWrapper>
+      <Underline />
+      <OffsetInput type='text' placeholder={placeholder} />
+    </Wrapper>
+  ;
 };
+
+const SIZES = {
+  small: {
+    '--fontSize': '14px',
+    iconStrokeWidth: 1,
+    '--underlineHeight': '1px',
+    '--leftOffset': '28px'
+  },
+  large: {
+    '--fontSize': '18px',
+    iconStrokeWidth: 2,
+    '--underlineHeight': '2px',
+    '--leftOffset': '36px'
+  }
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  width: var(--width);
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const Underline = styled.div`
+  position: absolute;
+  left: 2px;
+  right: 2px;
+  bottom: 2px;
+  background-color: black;
+  height: var(--underlineHeight);
+  pointer-events: none;
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 4px;
+  margin: auto;
+  height: min-content;
+  pointer-events: none;
+`
+
+const OffsetInput = styled.input`
+  width: 100%;
+  height: 100%;
+  padding: 7px 7px 7px var(--leftOffset);
+  border: none;
+  font-size: var(--fontSize);
+  color: ${COLORS.gray700};
+  font-weight: 700;
+
+  &::placeholder {
+    color:${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
 
 export default IconInput;
